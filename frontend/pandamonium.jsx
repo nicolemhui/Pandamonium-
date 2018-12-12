f4a4d84b346f71181e3d360bd5c9eea48fbd94b3
Kenneth Le <nicolemhui@gmail.com>
2018-12-11 23:06:13 -0800

    Add reducers and store

--- frontend/pandamonium.jsx
+++ frontend/pandamonium.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, signup, logout } from './util/session_api_util';
+//Components
+import configureStore from './store/store';
+
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store = configureStore();
+  
   const root = document.getElementById('root');
-
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
-
-  ReactDOM.render(<h1>I'M WORKING!</h1>, root);
+  ReactDOM.render(<h1>Hallo</h1>, root);
 });
 
-// import Root from './components/root';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { login, signup, logout } from './util/session_api_util';
 // import configureStore from './store/store';
-// const store = configureStore();
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const store = configureStore();
+//   const root = document.getElementById('root');
+  
+// //   window.login = login;
+// //   window.signup = signup;
+// //   window.logout = logout;
+
+// // // import Root from './components/root';
+
+
+//   ReactDOM.render(<h1>I'M WORKING!</h1>, root);
+// });
---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,9 @@
+//keeps track of all error messages
+import { combineReducers } from 'redux';
+import sessionErrorsReducer from './session_errors_reducer';
+
+const errorsReducer = combineReducers({
+  session: sessionErrorsReducer,
+});
+
+export default errorsReducer;
