d449b79aad1913e2e7a568ee896c1e0ed5463d6f
Kenneth Le <nicolemhui@gmail.com>
2018-12-11 16:45:39 -0800

    Set up Pandamonium project

---
+++ frontend/pandamonium.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>I'M WORKING!</h1>, root);
+});
+
+// import Root from './components/root';
+// import configureStore from './store/store';
+// const store = configureStore();
