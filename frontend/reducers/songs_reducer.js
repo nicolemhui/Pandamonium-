ba5d26138561b3429074ea5c3f8a3d12a560dfd9
Kenneth Le <nicolemhui@gmail.com>
2018-12-21 11:01:55 -0800

    fix songs reducer to merge state

--- frontend/reducers/songs_reducer.js
+++ frontend/reducers/songs_reducer.js
@@ -1,7 +1,7 @@
 import { RECEIVE_ALL_SONGS, RECEIVE_SONG } from '../actions/song_actions';
 import { RECEIVE_PLAYLIST } from '../actions/playlist_actions';
 import { RECEIVE_ALBUM } from '../actions/album_actions';
-import { RECEIVE_ARTIST } from '../actions/artist_actions';
+import { RECEIVE_ALL_ARTISTS, RECEIVE_ARTIST } from '../actions/artist_actions';
 import merge from 'lodash/merge';
 
 const songsReducer = (state = {}, action) => {
@@ -11,9 +11,8 @@ const songsReducer = (state = {}, action) => {
     case RECEIVE_PLAYLIST:
     case RECEIVE_ALBUM:
     case RECEIVE_ARTIST:
-      // if (action.payload.songs === undefined) return null;
-      return action.payload.songs;
-      // return merge({}, state, action.payload.songs);
+    case RECEIVE_ALL_ARTISTS:
+      return merge({}, state, action.payload.songs);
     case RECEIVE_SONG:
       return merge({}, state, action.payload.song);
     default:
