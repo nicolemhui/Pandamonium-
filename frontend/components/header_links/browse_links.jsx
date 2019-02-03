a4dc7daaa5338029f125130e0c140a21a48f79ca
Kenneth Le <nicolemhui@gmail.com>
2019-02-02 20:24:33 -0800

    Remove made-for-you links

--- frontend/components/header_links/browse_links.jsx
+++ frontend/components/header_links/browse_links.jsx
@@ -8,7 +8,7 @@ class BrowseLinks extends React.Component {
         <div className="nav-links-container">
           <div className="nav-bar-links">
             <NavLink to="/browse/playlists" className="nav-links-link">Playlists<span></span></NavLink>
-            <NavLink to="/browse/made-for-you" className="nav-links-link">Made for you<span></span></NavLink>
+            {/* <NavLink to="/browse/made-for-you" className="nav-links-link">Made for you<span></span></NavLink> */}
             <NavLink to="/browse/songs" className="nav-links-link">Songs<span></span></NavLink>
             <NavLink to="/browse/albums" className="nav-links-link">Albums<span></span></NavLink>
             <NavLink to="/browse/artists" className="nav-links-link">Artists<span></span></NavLink>
