68b69b078add6e35d5ebcd2da4748013e3187e97
Kenneth Le <nicolemhui@gmail.com>
2019-02-10 21:06:30 -0800

    Clean up code and remove comments

--- frontend/components/browse/browse_container.js
+++ frontend/components/browse/browse_container.js
@@ -2,8 +2,6 @@ import Browse from './browse';
 import { connect } from 'react-redux';
 
 const mapStateToProps = (state) => ({
-  //return playlists 
-  //FIX ME!
   currentUser: state.entities.users[state.session.id]
 });
 
